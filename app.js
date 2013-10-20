b0c8680fbf43fe1b4c11edf7a308750ecc990a3e
Toshiharu Takematsu <info@newfish.jp>
2013-10-20 09:34:47 +0900

    expressでwebrtc.ioもlistenするように

--- app.js
+++ app.js
@@ -8,5 +8,8 @@ var app = express();
 app.set('port', port);
  
 var server = http.createServer(app);
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
 
-var webRTC = require('webrtc.io').listen(port);
+var webRTC = require('webrtc.io').listen(server);

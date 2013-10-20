75277f11692a5a4728cca0ed3e835ea0486fe0b9
Toshiharu Takematsu <info@newfish.jp>
2013-10-20 09:28:55 +0900

    expressやっぱ追加。でport番号だけexpressにセットしてみる

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+var express = require('express');
+var http = require('http');
+
 var port = process.env.PORT || 8080;
+
+var app = express();
+
+app.set('port', port);
+ 
+var server = http.createServer(app);
 
 var webRTC = require('webrtc.io').listen(port);
